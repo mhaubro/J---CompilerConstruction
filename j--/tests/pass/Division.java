3494e471b4d8780dc3eb60ac5b1d198a84ed8801
egmeade <egmeade@live.com>
2018-02-19 16:56:48 +0100

    Committing the j-- compiler with div and rem implemented

---
+++ j--/tests/pass/Division.java
@@ -0,0 +1,14 @@
+package pass;
+
+import java.lang.System;
+
+public class Division {
+
+	public static void main (String[] args) {
+		System.out.println(20/7);
+	}
+
+	public static int divide(int x, int y) {
+		return x / y;
+	}
+}
---
+++ j--/tests/pass/Remainder.java
@@ -0,0 +1,15 @@
+package pass;
+
+import java.lang.System;
+
+public class Remainder {
+
+	public static void main(String[] args) {
+		System.out.println(7 % 4);
+	}
+
+	public int remainder(int x, int y) {
+		return x % y;
+	}
+
+}
