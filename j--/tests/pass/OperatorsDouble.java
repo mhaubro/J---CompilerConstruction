0536c395db688440cdaab431a54666aa0c997f80
Unknown <martin.haubro@hotmail.com>
2018-05-10 01:26:50 +0200

    DoubleTest of operators, int test

--- j--/tests/pass/OperatorsDouble.java
+++ j--/tests/pass/OperatorsDouble.java
@@ -4,6 +4,8 @@ import java.lang.System;
 
 public class OperatorsDouble {
 
+//	+=, -=, *=, /= %= %, /, +, -, *, (unary: +, -, -- ++, post and pre inc/dde
+
 	public static void main (String[] args) {
 		System.out.println(20.5/7.2);
 	}
@@ -28,5 +30,36 @@ public class OperatorsDouble {
 	public static double remAssign(double x, double y) {
 		x %= y;
 		return x;
+	}
+	public static double rem(double x, double y){
+		return x % y;
+	}
+	public static double div(double x, double y){
+		return x/y;
+	}
+	public static double plus(double x, double y){
+		return x+y;
+	}
+	public static double minus(double x, double y){
+		return x-y;
+	}
+	public static double mul(double x, double y){
+		return x*y;
+	}
+	public static double postdec(double x){
+		x--;
+		return x--;
+	}
+	public static double predec(double x){
+		--x;
+		return --x;
+	}
+	public static double postinc(double x){
+		x++;
+		return x++;
+	}
+	public static double preinc(double x){
+		++x;
+		return ++x;
 	}
 }
