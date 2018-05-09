7c13d4335de30e4f8111a45daa08dceff579a8fa
erikgroving <egmeade@live.com>
2018-05-09 22:01:44 +0200

    Proper disambiguation between fors and made the double test passable. double literal slight change to move the or in the scanner and not the parser

--- j--/tests/pass/Double.java
+++ j--/tests/pass/Double.java
@@ -1,7 +1,8 @@
 package pass;
+import java.lang.System;
 
 public class Double {
-    //static double[] a = {1., 2., 3., 4., 5., 6., 7., 8., 9., 10.};
+    static double[] a = {1., 2., 3., 4., 5., 6., 7., 8., 9., 10.};
 
     public static void main(String[] args) {
         double sum1 = .5;
